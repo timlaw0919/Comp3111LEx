9748a070a368930d56069c62f2099315bb887a1f
timlaw0919 <hnlaw@connect.ust.hk>
2023-09-12 21:13:25 +0800

    Add the 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -5,6 +5,7 @@ public class mainApp1 {
     {
         int b = 2;
         int n = 11;
+        System.out.println("It is hard to setup :(");
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program started ...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b, n));
